f7e4af6bad2017c591ccee056a6d41f643d10de0
Scarlett Patton <scarwars05@gmail.com>
2018-06-21 20:49:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,10 +8,20 @@ function nestedTarget(target) {
   return document.querySelector('.target');
 }
 
-// function increaseRankBy(n) {
-//   return document.getElementById('.ranked-list')
-//   for (i = 1, i < ranked-list.length , i++) {
-// }
+increaseRankBy(3)
+const rankedLists = document.querySelectorAll('.ranked-list')
+const firstList = rankedLists[0]
+const secondList = rankedLists[1]
+let children = firstList.children
+let start = 1
+for (let i = 0, l = children.length; i < l; i++) {
+  expect(parseInt(children[i].innerHTML)).toEqual(start + i + 3)
+}
+children = secondList.children
+start = 12
+for (let i = 0, l = children.length; i < l; i++) {
+  expect(parseInt(children[i].innerHTML)).toEqual(start - i + 3)
+}
  
 function deepestChild(child) {
   return document.querySelector('#grand-node div div div div');
