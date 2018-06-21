096d0d14cb1f94bcf7b439d990158de841eefb03
Scarlett Patton <scarwars05@gmail.com>
2018-06-21 20:55:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function nestedTarget(target) {
 
 function increaseRankBy(number) {
   return document.querySelector('.ranked-list');
-  for ()
+  for (i = 0, i > 1, i++);
 }
 
 function deepestChild(child) {
