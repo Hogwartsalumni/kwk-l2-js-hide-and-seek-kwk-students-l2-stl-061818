60f2fb6cf597029bd39ea65478e41d82442268ae
Scarlett Patton <scarwars05@gmail.com>
2018-06-21 21:16:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function nestedTarget(target) {
 function increaseRankBy(n) {
 const rank = document.querySelectorAll('ul.ranked-list');
 
-  for (let i = 0; i < rank.length; i++) {
+for (let i = 0; i < rank.length; i++) {
     rank[i].innerHTML = (i + 1).toString();
   }
 
