2c14f25982f560d4404ed3d1342adb93970cccf9
Scarlett Patton <scarwars05@gmail.com>
2018-06-21 20:48:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 //Code your solution here
+
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget(target) {
+  return document.querySelector('.target');
+}
+
+function increaseRankBy(n) {
+  return document.getElementById('.ranked-list')
+  for (i = 1, i < ranked-list.length , i++) {
+}
+ 
+function deepestChild(child) {
+  return document.querySelector('#grand-node div div div div');
+}
