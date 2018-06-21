b4aab14bb6bbf4f3bea34d13cc34ba893985d59c
Scarlett Patton <scarwars05@gmail.com>
2018-06-21 21:17:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,8 +14,7 @@ const rank = document.querySelectorAll('ul.ranked-list');
 for (let i = 0; i < rank.length; i++) {
     rank[i].innerHTML = (i + 1).toString();
   }
-
+}
 function deepestChild(child) {
   return document.querySelector('#grand-node div div div div');
-}
 }
